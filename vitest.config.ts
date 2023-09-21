bbcdf4411671cccaf61a100fcfbdb512d4337201
MasatoMakino <makino.masato.g@gmail.com>
2023-09-21 16:08:48 +0900

    change : threads false
    jsdom + canvas を利用する場合、マルチスレッド化するとCanvas要素の利用に失敗する。

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     name: "jsdomTest",
     environment: "jsdom",
+    threads: false,
     coverage: {
       provider: "istanbul",
       reporter: ["text", "html", "json", "lcov"],
